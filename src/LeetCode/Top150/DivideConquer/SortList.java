ea7df650728bd2fa2a23f5e862a98bc73aa2a3bb
hankil_youn <onewayy91@gmail.com>
2025-01-11 20:28:36 +0900

    feat: refactor

--- src/LeetCode/Top150/DivideConquer/SortList.java
+++ src/LeetCode/Top150/DivideConquer/SortList.java
@@ -2,25 +2,41 @@ package LeetCode.Top150.DivideConquer;
 
 
 public class SortList {
-    static class Solution {
-        public ListNode sortList(ListNode head) {
-            if (head == null || head.next == null) {
-                return head;
-            }
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        ListNode node1 = new ListNode(4);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(1);
+        ListNode node4 = new ListNode(3);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
 
-            ListNode mid = getMid(head);
-            ListNode tmp = mid.next;
-            mid.next = null;
+        ListNode listNode = solution.sortList(node1);
+        System.out.println(listNode);
 
-            ListNode left = head;
-            ListNode right = tmp;
-            left = this.sortList(left);
-            right = this.sortList(right);
-            return merge(left, right);
+
+    }
+}
+
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
         }
+
+        ListNode mid = getMid(head);
+        ListNode tmp = mid.next;
+        mid.next = null;
+
+        ListNode left = head;
+        ListNode right = tmp;
+        left = this.sortList(left);
+        right = this.sortList(right);
+        return merge(left, right);
     }
 
-    public static ListNode getMid(ListNode node) {
+    private ListNode getMid(ListNode node) {
         ListNode slow = node;
         ListNode fast = node.next;
         while (fast != null && fast.next != null) {
@@ -30,7 +46,7 @@ public class SortList {
         return slow;
     }
 
-    public static ListNode merge(ListNode node1, ListNode node2) {
+    private ListNode merge(ListNode node1, ListNode node2) {
         ListNode dummy = new ListNode();
         ListNode tail = dummy;
 
@@ -48,22 +64,6 @@ public class SortList {
         tail.next = (node1 != null) ? node1 : node2;
 
         return dummy.next;
-    }
-
-    public static void main(String[] args) {
-        SortList.Solution solution = new SortList.Solution();
-        ListNode node1 = new ListNode(4);
-        ListNode node2 = new ListNode(2);
-        ListNode node3 = new ListNode(1);
-        ListNode node4 = new ListNode(3);
-        node1.next = node2;
-        node2.next = node3;
-        node3.next = node4;
-
-        ListNode listNode = solution.sortList(node1);
-        System.out.println(listNode);
-
-
     }
 }
 
