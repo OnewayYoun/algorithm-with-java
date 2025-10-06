c847976b676023c5bc6a8fadcbd6986c72c7192f
hankil_youn <onewayy91@gmail.com>
2025-10-06 19:18:18 +0900

    ps: solve with while loop

--- src/파이썬알고리즘인터뷰/문자열조작/leetcode125/Solution.java
+++ src/파이썬알고리즘인터뷰/문자열조작/leetcode125/Solution.java
@@ -12,6 +12,21 @@ class Solution {
         return IntStream.range(0, newString.length() / 2).allMatch(i -> newString.charAt(i) == newString.charAt(newString.length() - i - 1));
     }
 
+    public boolean isPalindrome1(String s) {
+        String newString = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+        int left = 0;
+        int right = newString.length() - 1;
+
+        while (left < right) {
+            if (newString.charAt(left) != newString.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         Solution solution = new Solution();
         System.out.println(solution.isPalindrome("0z;z   ; 0"));
