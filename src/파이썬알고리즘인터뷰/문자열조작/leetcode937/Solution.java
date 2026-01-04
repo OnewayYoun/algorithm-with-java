38146d7dc4f95607148dac673822e589fa03fb0d
hankil_youn <onewayy91@gmail.com>
2026-01-04 20:23:42 +0900

    ps: solve again with implementing Comparator

--- src/파이썬알고리즘인터뷰/문자열조작/leetcode937/Solution.java
+++ src/파이썬알고리즘인터뷰/문자열조작/leetcode937/Solution.java
@@ -1,32 +1,22 @@
 package 파이썬알고리즘인터뷰.문자열조작.leetcode937;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 class Solution {
     public String[] reorderLogFiles(String[] logs) {
-        List<String> letterLogs= new ArrayList<>();
-        List<String> digitLogs= new ArrayList<>();
-        for (String s: logs) {
-            String[] log = s.split(" ");
-            if (Character.isAlphabetic(log[1].charAt(0))) {
-                letterLogs.add(s);
-            } else {
-                digitLogs.add(s);
+        List<String> letterLogs = new ArrayList<>();
+        List<String> digitLogs = new ArrayList<>();
+
+        for (String log : logs) {
+            String[] split = log.split(" ", 2);
+            if (Character.isLetter(split[1].charAt(0))) {
+                letterLogs.add(log);
+            } else if (Character.isDigit(split[1].charAt(0))) {
+                digitLogs.add(log);
             }
         }
-        Collections.sort(letterLogs, (a, b) ->{
-            String[] splitA = a.split(" ", 2);
-            String[] splitB = b.split(" ", 2);
 
-            int cmp = splitA[1].compareTo(splitB[1]);
-            if (cmp == 0) {
-                return splitA[0].compareTo(splitB[0]);
-            }
-            return cmp;
-        });
-
+        letterLogs.sort(new LetterLogComparator());
         List<String> result = new ArrayList<>();
         result.addAll(letterLogs);
         result.addAll(digitLogs);
@@ -37,5 +27,20 @@ class Solution {
     public static void main(String[] args) {
         Solution solution = new Solution();
         solution.reorderLogFiles(new String[]{"dig1 8 1 5 1", "let1 art can", "dig2 3 6", "let2 own kit dig", "let3 art zero"});
+    }
+}
+
+class LetterLogComparator implements Comparator<String> {
+
+    @Override
+    public int compare(String o1, String o2) {
+        String[] split1 = o1.split(" ", 2);
+        String[] split2 = o2.split(" ", 2);
+
+        int cmp = split1[1].compareTo(split2[1]);
+        if (cmp != 0) {
+            return cmp;
+        }
+        return split1[0].compareTo(split2[0]);
     }
 }
