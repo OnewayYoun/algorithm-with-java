e0df213a5a541dc403b361d38ff22cb2c823c19d
hankil_youn <onewayy91@gmail.com>
2026-01-25 18:54:58 +0900

    ps: twoSum(leetcode 1)

---
+++ src/파이썬알고리즘인터뷰/문자열조작/leetcode001/Solution.java
@@ -0,0 +1,23 @@
+package 파이썬알고리즘인터뷰.문자열조작.leetcode001;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (numMap.containsKey(target - nums[i])) {
+                return new int[]{numMap.get(target - nums[i]), i};
+            }
+            numMap.put(nums[i], i);
+        }
+        return new int[0];
+    }
+
+    public static void main(String[] arg) {
+        Solution solution = new Solution();
+        System.out.println(Arrays.toString(solution.twoSum(new int[]{2, 7, 11, 15}, 9)));
+    }
+}
